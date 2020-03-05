917fe96e77ea896bad3f59be87a36b21ac6ff974
Henry <hoangtran194@gmail.com>
2020-03-05 16:52:34 -0500

    [Flutter] Initialize

---
+++ fitcon/android/app/src/main/kotlin/comp231/fitcon/MainActivity.kt
@@ -0,0 +1,12 @@
+package comp231.fitcon
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
