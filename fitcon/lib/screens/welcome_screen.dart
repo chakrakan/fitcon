600e5ee573a6c14715615c57aed7b4656ad52e02
Matthew <matthew.r.cox70@gmail.com>
2020-03-06 11:40:38 -0500

    Created welcome page - added assets/components - commented out firebase dependencies in .yaml file due to launch error

---
+++ fitcon/lib/screens/welcome_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:fitcon/components/RoundedButton.dart';
+import 'login_screen.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static const String id = 'welcome_screen';
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller =
+        AnimationController(duration: Duration(seconds: 1), vsync: this);
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white)
+        .animate(controller);
+    controller.forward();
+    controller.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: animation.value,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/login_bg.png"),
+              fit: BoxFit.cover),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Hero(
+                    tag: 'logo',
+                    child: Container(
+                        child: Image.asset('assets/images/login_logo.png'),
+                        height: 100.0,
+                        width: 350.0,
+                        margin: EdgeInsets.fromLTRB(0, 0, 0, 150)),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 48.0,
+              ),
+              RoundedButton(
+                title: 'Log In',
+                colour: Colors.lightBlueAccent,
+                onPressed: () {
+                  //Navigator.pushNamed(context, LoginScreen.id);
+                },
+              ),
+              RoundedButton(
+                title: 'Register',
+                colour: Colors.blueAccent,
+                onPressed: () {
+                  //Navigator.pushNamed(context, RegistrationScreen.id);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
