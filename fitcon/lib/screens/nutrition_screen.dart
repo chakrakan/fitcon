4da12100054f64994b2982991716ed2ba3bf457a
Matthew <matthew.r.cox70@gmail.com>
2020-03-27 17:53:42 -0400

    Added "Add new" button and a popup form field

--- fitcon/lib/screens/nutrition_screen.dart
+++ fitcon/lib/screens/nutrition_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:dropdown_formfield/dropdown_formfield.dart';
 import '../components/RoundedButton.dart';
 import 'main_screen.dart';
 
@@ -11,6 +11,27 @@ class NutritionScreen extends StatefulWidget {
 }
 
 class _NutritionScreenState extends State<NutritionScreen> {
+  String _myDay;
+  String _myDayResult;
+  final formKey = new GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _myDay = '';
+    _myDayResult = '';
+  }
+
+  _saveForm() {
+    var form = formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      setState(() {
+        _myDayResult = _myDay;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,13 +41,14 @@ class _NutritionScreenState extends State<NutritionScreen> {
           primarySwatch: Colors.blue,
         ),
         home: Scaffold(
-          appBar: AppBar(
-            title: new Text(
-              'Nutrition Plan',
+            appBar: AppBar(
+              title: new Text(
+                'Nutrition Plan',
+              ),
             ),
-          ),
             body: Column(
               children: <Widget>[
+                //Build Tiles
                 Expanded(
                   child: ListView.builder(
                     itemBuilder: (BuildContext context, int index) =>
@@ -34,16 +56,103 @@ class _NutritionScreenState extends State<NutritionScreen> {
                     itemCount: data.length,
                   ),
                 ),
-                RoundedButton(
-                    title: 'Return',
-                    colour: Colors.blueAccent,
-                    onPressed: () {
-                      Navigator.pushNamed(context, MainScreen.id);
-                    }
-                )
+                //Display Return and New buttons
+                Row(
+                  children: <Widget>[
+                    RoundedButton(
+                        title: 'Return',
+                        colour: Colors.blueAccent,
+                        onPressed: () {
+                          Navigator.pushNamed(context, MainScreen.id);
+                        }),
+                    Column(
+                      children: <Widget>[
+                        RoundedButton(
+                            title: 'Add new',
+                            colour: Colors.blueAccent,
+                            onPressed: () {
+                              showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    //Start Popup Form
+                                    return AlertDialog(content: StatefulBuilder(
+                                        builder: (BuildContext context,
+                                            StateSetter setState) {
+                                          return Column(
+                                              mainAxisSize: MainAxisSize.min,
+                                              children: <Widget>[
+                                                Padding(
+                                                  padding: EdgeInsets.all(8.0),
+                                                  child: DropDownFormField(
+                                                    titleText: 'Day of Week',
+                                                    hintText: _myDay,
+                                                    value: _myDay,
+                                                    onSaved: (value) {
+                                                      setState(() {
+                                                        _myDay = value;
+                                                      });
+                                                    },
+                                                    onChanged: (value) {
+                                                      setState(() {
+                                                        _myDay = value;
+                                                      });
+                                                    },
+                                                    dataSource: [
+                                                      {
+                                                        "display": "Sunday",
+                                                        "value": "sun",
+                                                      },
+                                                      {
+                                                        "display": "Monday",
+                                                        "value": "mon",
+                                                      },
+                                                      {
+                                                        "display": "Tuesday",
+                                                        "value": "tue",
+                                                      },
+                                                      {
+                                                        "display": "Wednesday",
+                                                        "value": "wed",
+                                                      },
+                                                      {
+                                                        "display": "Thursday",
+                                                        "value": "thu",
+                                                      },
+                                                      {
+                                                        "display": "Friday",
+                                                        "value": "fri",
+                                                      },
+                                                      {
+                                                        "display": "Saturday",
+                                                        "value": "sat",
+                                                      },
+                                                    ],
+                                                    textField: 'display',
+                                                    valueField: 'value',
+                                                  ),
+                                                ),
+                                                Padding(
+                                                  padding: EdgeInsets.all(8.0),
+                                                  child: TextFormField(),
+                                                ),
+                                                Container(
+                                                  padding: EdgeInsets.all(8),
+                                                  child: RoundedButton(
+                                                    title: 'Save',
+                                                    colour: Colors.blueAccent,
+                                                    onPressed: _saveForm,
+                                                  ),
+                                                ),
+                                              ]);
+                                        }));
+                                  });
+                            }),
+                      ],
+                    ),
+                  ],
+                ),
               ],
-            )
-        ));
+            )));
   }
 }
 
@@ -125,7 +234,6 @@ class EntryItem extends StatelessWidget {
       key: PageStorageKey<Entry>(root),
       title: Text(root.title),
       children: root.children.map(_buildTiles).toList(),
-
     );
   }
 
