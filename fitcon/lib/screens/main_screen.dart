81b21a35e38aa26c0adf83e71f32669b81ff2979
Kanisk <57884127+chakrakan@users.noreply.github.com>
2020-03-31 07:15:04 -0400

    update

--- fitcon/lib/screens/main_screen.dart
+++ fitcon/lib/screens/main_screen.dart
@@ -6,7 +6,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:fitcon/screens/Edit_Screen.dart';
+import 'package:fitcon/screens/edit_profile_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:fluttertoast/fluttertoast.dart';
